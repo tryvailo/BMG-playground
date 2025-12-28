1d6cde4b8795ab6535700b888ef25bf9ba1e05a6
tryvailo <tryvailo@gmail.com>
2025-12-28 21:11:05 +0000

    fix: добавление правила caughtErrorsIgnorePattern в ESLint конфигурацию

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,4 +1,20 @@
 import eslintConfigApps from '@kit/eslint-config/apps.js';
 import eslintConfigBase from '@kit/eslint-config/base.js';
 
-export default [...eslintConfigBase, ...eslintConfigApps];
+export default [
+  ...eslintConfigBase,
+  ...eslintConfigApps,
+  {
+    rules: {
+      // Разрешить неиспользуемые переменные в catch блоках с префиксом _
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_', // Ключевое правило: разрешить _error в catch
+        },
+      ],
+    },
+  },
+];
